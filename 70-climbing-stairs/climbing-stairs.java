700be0f133373e263a61dc42cf78706911232877
DHAIRYA GOEL <146471708+DhairyaGoel05@users.noreply.github.com>
2025-02-10 12:01:25 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (64.70%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        int dp[]=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];    
+        
+    }
+    // public int rec(int n,int[] dp) {
+    //     if(n<=2){
+    //         dp[n]=n;
+    //         return n;
+    //     }
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     dp[n]=climbStairs(n-1)+climbStairs(n-2);
+    //     return dp[n];
+    // }
+}
